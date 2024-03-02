20f480f0502eaf54719194bf7f3854ee7e054b2a
Maryam Tanvir <maryamchaudhry318@gmail.com>
2024-03-03 02:43:25 +0500

    first

---
+++ src/components/Users/Controllers/main.js
@@ -0,0 +1,48 @@
+const getTableData = (req, res, db) => {
+    db.select('*').from('public.test')
+      .then(items => {
+        if(items.length){
+          res.json(items)
+        } else {
+          res.json({dataExists: 'false'})
+        }
+      })
+      .catch(err => res.status(400).json({dbError: 'db error'}))
+  }
+  
+  const postTableData = (req, res, db) => {
+    const { email, username, password} = req.body
+    const added = new Date()
+    db('public.test').insert({email, username, password, added})
+      .returning('*')
+      .then(item => {
+        res.json(item)
+      })
+      .catch(err => res.status(400).json({dbError: 'db error'}))
+  }
+  
+  const putTableData = (req, res, db) => {
+    const { id, email, username, password} = req.body
+    db('public.test').where({id}).update({email, username, password})
+      .returning('*')
+      .then(item => {
+        res.json(item)
+      })
+      .catch(err => res.status(400).json({dbError: 'db error'}))
+  }
+  
+  const deleteTableData = (req, res, db) => {
+    const { id } = req.body
+    db('public.test').where({id}).del()
+      .then(() => {
+        res.json({delete: 'true'})
+      })
+      .catch(err => res.status(400).json({dbError: 'db error'}))
+  }
+  
+  module.exports = {
+    getTableData,
+    postTableData,
+    putTableData,
+    deleteTableData
+  }
---
+++ src/components/Users/Users-app/App.js
@@ -0,0 +1,137 @@
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col } from 'reactstrap';
+
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [editableEmail, setEditableEmail] = useState(null);
+  const [updatedEmail, setUpdatedEmail] = useState('');
+
+  const getItems = () => {
+    fetch('http://localhost:5000/crud')
+      .then((response) => response.json())
+      .then((data) => setItems(data))
+      .catch((error) => console.log(error));
+  };
+
+  const addItemToState = (item) => {
+    setItems((prevItems) => [...prevItems, item]);
+  };
+
+  const updateState = (item) => {
+    const updatedItems = items.map((data) => (data.id === item.id ? item : data));
+    setItems(updatedItems);
+    setEditableEmail(null);
+    setUpdatedEmail('');
+  };
+
+  const deleteItemFromState = (id) => {
+    const updatedItems = items.filter((item) => item.id !== id);
+    setItems(updatedItems);
+  };
+
+  const handleEditClick = (email) => {
+    setEditableEmail(email);
+    setUpdatedEmail(email);
+  };
+
+  const handleUpdateClick = (id) => {
+    fetch('http://localhost:5000/crud', {
+      method: 'put',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        id,
+        email: updatedEmail,
+      }),
+    })
+      .then((response) => response.json())
+      .then((item) => {
+        if (Array.isArray(item)) {
+          updateState(item[0]);
+        } else {
+          console.log('Update failure');
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const handleDeleteClick = (id) => {
+    fetch('http://localhost:5000/crud', {
+      method: 'delete',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        id,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.delete === 'true') {
+          deleteItemFromState(id);
+        } else {
+          console.log('Delete failure');
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    getItems();
+  }, []);
+
+  return (
+    <Container className="App">
+      <Row>
+        <Col>
+          <h1 style={{ margin: '20px 0' }}>CRUD Database</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <table style={{ width: '100%', borderSpacing: '10px' }}>
+            <thead>
+              <tr>
+                <th>Email</th>
+                <th>Username</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {items.map((item) => (
+                <tr key={item.id}>
+                  <td>
+                    {editableEmail === item.email ? (
+                      <input
+                        type="text"
+                        value={updatedEmail}
+                        onChange={(e) => setUpdatedEmail(e.target.value)}
+                      />
+                    ) : (
+                      item.email
+                    )}
+                  </td>
+                  <td>{item.username}</td>
+                  <td>
+                    {editableEmail === item.email ? (
+                      <button onClick={() => handleUpdateClick(item.id)}>Update</button>
+                    ) : (
+                      <>
+                        <button onClick={() => handleEditClick(item.email)}>Edit</button>
+                        <button onClick={() => handleDeleteClick(item.id)}>Delete</button>
+                      </>
+                    )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </Col>
+      </Row>
+  
+    </Container>
+  );
+};
+
+export default App;
